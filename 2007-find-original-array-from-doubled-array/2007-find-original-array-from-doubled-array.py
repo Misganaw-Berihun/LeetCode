1722d48f232071324762d142c661c335f06b6ba2
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-01-18 14:46:05 +0300

    Update 2007-find-original-array-from-doubled-array.py

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -8,7 +8,7 @@ class Solution:
         for val in changed:
             freq = count[val] 
             
-            if freq:#are there any unused values of val?
+            if freq:#are there any unused occurences of val?
                 count[val] -= 1 #is used hence subtract 1 from freq of val
                 double = 2 * val
                 double_idx = count.get(double)
@@ -18,7 +18,7 @@ class Solution:
                     count[double] -= 1 #doulbe is used hence subtract 1 from freq of double
                 else: #val * 2 not in changed
                     return []
-            else:  #if all values of val are used
+            else:  #if all occurences of val are used
                 continue
                 
         return ans
