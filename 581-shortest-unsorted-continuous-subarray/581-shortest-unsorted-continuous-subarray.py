2e625ba2120323bc15bf6ac11f5291b0ab742ee8
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-01-25 19:14:35 +0300

    Time: 461 ms (5.03%), Space: 15.4 MB (70.04%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -7,26 +7,20 @@ class Solution:
         
         
         for i in range(len(nums)):
-            if not stack1 or nums[i] > nums[stack1[-1]]:
-                stack1.append(i)
-            else:
-                while stack1 and nums[stack1[-1]] > nums[i]:
+            while stack1 and nums[stack1[-1]] > nums[i]:
                     left = min(stack1.pop(),left)
-                    
-                stack1.append(i)
+            stack1.append(i)
     
                     
         for j in range(len(nums) - 1, -1,-1):
-            if not stack2 or nums[j] < nums[stack2[-1]]:
-                stack2.append(j)
-            else:
-                while stack2 and nums[stack2[-1]] < nums[j]:
+            while stack2 and nums[stack2[-1]] <nums[j]:
                     right = max(stack2.pop(),right)
-                    
-                stack2.append(j)
+            
+            stack2.append(j)
                     
         if right == -1 and left == len(nums):
             return 0
+        
         return right - left + 1
             
                     
