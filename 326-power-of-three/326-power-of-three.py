7b33f67d37589d90d4bf9f7503607da754856e4b
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-01-29 13:06:59 +0300

    Update 326-power-of-three.py

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        if n <= 3:
-            return n == 3 or n == 1;
-        elif n % 3 != 0:
-            return False
+        if n % 3 == 0:
+            return self.isPowerOfThree(n/3)
         
-        return self.isPowerOfThree(n/3)
+        return n == 1
         
