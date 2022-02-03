f68c1321c604a330fbfe895a596ff9521451a2b7
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-03 15:18:43 +0300

    Time: 52 ms (17.67%), Space: 13.8 MB (96.47%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def dp(i):
+            if i <= 2:
+                return i
+            if i not in memo:
+                memo[i] = dp(i-1) + dp(i-2)
+            
+            
+            return memo[i] 
+        
+        memo = {}
+        return dp(n)
+            
+        
