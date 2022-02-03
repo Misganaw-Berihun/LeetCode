976faa9558933789ee2a4748480b5a3175886c69
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-03 15:20:05 +0300

    Time: 49 ms (23.50%), Space: 14 MB (90.08%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,15 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def dp(i):
-            if i <= 2:
-                return i
-            if i not in memo:
-                memo[i] = dp(i-1) + dp(i-2)
+        if n == 1:
+            return 1
             
-            
-            return memo[i] 
+        # An array that represents the answer to the problem for a given state
+        dp = [0] * (n + 1)
+        dp[1] = 1 # Base cases
+        dp[2] = 2 # Base cases
         
-        memo = {}
-        return dp(n)
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] # Recurrence relation
+
+        return dp[n]
             
         
