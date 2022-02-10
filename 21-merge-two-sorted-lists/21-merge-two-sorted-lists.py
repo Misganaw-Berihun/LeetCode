b31bcafc736437b86dc858bc3a4e66efb8e8b052
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-10 13:50:16 +0300

    Time: 59 ms (31.89%), Space: 13.9 MB (96.57%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -5,49 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        p1 = list1
-        p2 = list2
-        p = None
-        cur = None
-        
-        if not list1 and not list2:
-            return None
-        
-        if (p1 and not p2):
-            return p1
-            
-        elif (p2 and not p1):
-            return p2
-        
-        if  (p1.val < p2.val):
-            p = p1
-            cur = p1
-            p1 = p1.next
+        if list1 == None:
+            return list2
+        if list2 == None:
+            return list1
+        if list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next,list2)
+            return list1
         else:
-            p = p2
-            cur = p2
-            p2 = p2.next
-            
-            
-        while p1 or p2:
-            if p1 and p2:
-                if p1.val < p2.val:
-                    cur.next = p1
-                    p1 = p1.next
-                    cur = cur.next
-                else:
-                    cur.next = p2
-                    p2 = p2.next
-                    cur = cur.next
-            elif not p2:
-                cur.next = p1
-                break
-            elif not p1:
-                cur.next = p2   
-                break
-                
-            
-        return p
+            list2.next = self.mergeTwoLists(list1,list2.next)
+            return list2
                     
         
                 
