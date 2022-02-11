dbb8159cdd3d772404ea034c3b4ade9a4061447e
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-11 13:41:46 +0300

    Time: 91 ms (52.06%), Space: 14 MB (98.70%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -5,15 +5,13 @@ class Solution:
         idx = 0
         
         for i,ch in enumerate(s):
-            if ch in occur.keys():
-                longest = max(longest , i - idx)
-                
-                if occur[ch] >= idx:
-                    idx = occur[ch] + 1
+            if ch in occur.keys() and occur[ch] >= idx:
+                idx = occur[ch] + 1
+            else:
+                longest = max(longest,i-idx + 1)
             
             occur[ch] = i
         
-        longest = max(longest,len(s) - idx)
                 
         return longest
         
