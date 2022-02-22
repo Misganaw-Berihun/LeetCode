17c325ff12552f8ce04179a357f63f9d9b9deeb9
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-22 15:49:54 +0300

    Time: 65 ms (6.42%), Space: 13.8 MB (87.77%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -3,12 +3,10 @@ class Solution:
         if n == 1:
             return 5
         i = n // 2
+        res = pow(20,i,1000000007)
         
         if n%2:
-            res = (pow(20,i,1000000007))
             res = res * 5  % 1000000007
-        else:
-            res = pow(20,i,1000000007)
         
         return res
         
