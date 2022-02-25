2f770218e890c9c333f453bee9373c0786a27c7d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-25 16:02:01 +0300

    Time: 968 ms (63.56%), Space: 19.5 MB (99.30%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        occurence  = defaultdict(int)
+        i = 0
+        ret = 0
+        length = 0
+        
+        for j in range(len(fruits)):
+            occurence[fruits[j]] += 1
+            
+            if len(occurence) > 2:
+                while i <= j and i < len(fruits) and occurence[fruits[i]] != 1:
+                    occurence[fruits[i]] -= 1
+                    i += 1
+                
+                if i < len(fruits):
+                    occurence.pop(fruits[i])
+                    
+                length = j - i
+                i += 1
+            else:
+                length += 1
+                ret = max(ret,length)
+        
+        return ret
+        
+        
+        
+            
+                
+            
+        
