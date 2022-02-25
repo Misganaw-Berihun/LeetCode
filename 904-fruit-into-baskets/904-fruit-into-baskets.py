957fdc5e5497ada845a5460e07aa4d4b2ff490c2
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-02-25 17:27:30 +0300

    Time: 892 ms (72.51%), Space: 20 MB (77.33%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -3,22 +3,19 @@ class Solution:
         occurence  = defaultdict(int)
         i = 0
         ret = 0
-        length = 0
         
         for j in range(len(fruits)):
             occurence[fruits[j]] += 1
             
             if len(occurence) > 2:
-                while i < len(fruits) and occurence[fruits[i]] != 1:
+                while occurence[fruits[i]] != 1:
                     occurence[fruits[i]] -= 1
                     i += 1
                 
                 occurence.pop(fruits[i]) 
                 length = j - i
                 i += 1
-            else:
-                length += 1
-                ret = max(ret,length)
+            ret = max(ret,j - i + 1)
         
         return ret
         
