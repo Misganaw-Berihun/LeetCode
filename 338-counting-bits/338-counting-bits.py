8ffd01f344a4a7412bdf028819af02b25f28264f
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-01 22:11:17 +0300

    Time: 84 ms (88.58%), Space: 20.8 MB (52.29%) - LeetHub

--- 338-counting-bits/338-counting-bits.py
+++ 338-counting-bits/338-counting-bits.py
@@ -1,20 +1,15 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        acc = [0,1,1,2]
-        
-        if n <= 3:
-            return acc[0:n+1]
-        else:
-            pow = 2
+        acc = [0] * (n+1)
+        offset = 1
+
+        for i in range(1,n+1):
+            if i == 2*offset:
+                offset *= 2
             
-            for i in range(4,n+1):
-                if i == 2*pow:
-                    acc.append(1)
-                    pow *= 2
-                else:
-                    dif = i - pow
-                    acc.append( 1 + acc[dif])
-                    
-                    
+            dif = i - offset
+            acc[i] = 1 + acc[dif]
+
+
         return acc
                     
