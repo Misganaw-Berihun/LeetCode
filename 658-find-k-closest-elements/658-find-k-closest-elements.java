bfb4aa2446ff324cb55d4091316aff9264aaa00f
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-22 15:21:13 +0300

    Time: 7 ms (56.30%), Space: 62.3 MB (55.76%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,62 +1,26 @@
 class Solution {
-    public static int binarySearch(int [] arr,int target){
-        int left = 0,right = arr.length - 1;
+    public static int binarySearch(int [] arr,int target,int k){
+        int left = 0,right = arr.length - k;
         
-        while (left <= right){
+        while (left < right){
             int mid = left +( right - left )/ 2;
             
-            if (arr[mid] > target){
-                right  = mid - 1;
-            }
-            else if (arr[mid] < target){
-                left = mid + 1;
+            if (target - arr[mid]  <= arr[mid + k] - target){
+                right  = mid;
             }else{
-                return mid;
+                left = mid + 1;
             }
         }
         
         return left;
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int pivot = binarySearch(arr,x);
-        int n = arr.length;
-        List<Integer> ans = new ArrayList<Integer>();
-        int left_ptr = 0, right_ptr = n - 1;
+     int left_ptr = binarySearch(arr,x,k);
+     List<Integer> ans = new ArrayList<Integer>();
         
-        if (pivot  < 0){pivot = 0;}
-        else if (pivot == n){pivot = n - 1;}
-        
-        if (arr[pivot] == x){left_ptr = pivot;right_ptr = pivot + 1;}
-        else if(arr[pivot] != x){left_ptr = pivot - 1 ;right_ptr = pivot;}
-        
-        while ((left_ptr >= 0 && right_ptr < n)  && k > 0){
-            int left_dist = Math.abs(arr[left_ptr] - x);
-            int right_dist = Math.abs(arr[right_ptr] - x);
-            
-            if (left_dist <= right_dist){
-                left_ptr--;
-            }else{
-                right_ptr++;
-            }
-            k--;
-        }
-       
-        while (k > 0 && left_ptr >= 0){
-            left_ptr--;
-            k--;
-        }
-        
-        while(k>0 && right_ptr < n){
-            right_ptr++;
-            k--;
-        }
-        
-        left_ptr = left_ptr >= 0? left_ptr: -1;
-        right_ptr = right_ptr < n? right_ptr: n;
-        
-        for(int i = left_ptr + 1; i < right_ptr; i++){
-            ans.add(arr[i]);
-        }
+    for(int i = left_ptr; i < left_ptr + k; i++){
+        ans.add(arr[i]);
+    }
      return ans;   
     }
 }
