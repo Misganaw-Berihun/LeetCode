c727d462e46dbd95a3b0a9b563c42a7010625068
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-23 10:22:47 +0300

    Time: 10 ms, Memory: 44.3 MB - LeetHub

--- intersection-of-two-arrays/intersection-of-two-arrays.java
+++ intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,36 +1,33 @@
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
-        int [] visited = new int[1001];
-        Arrays.fill(visited,-1);
-        List<Integer> ans = new ArrayList<Integer>();
+    public static boolean binarySearch(int [] nums,int target){
+        int n = nums.length,left = 0,right = n - 1;
         
-        for(int num:nums1){
-            visited[num] = 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > target){
+                right = mid - 1;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                return true;
+            }
         }
+        
+        return false;
+    }
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Set<Integer> visited = new HashSet<Integer>();
+        Arrays.sort(nums1);
         
         for(int num: nums2){
-            if (visited[num] >= 0){
-                visited[num] = 0;
+            if(!visited.contains(num) && binarySearch(nums1,num)){
+                visited.add(num);
+                ans.add(num);
             }
         }
         
-        for(int num: nums1){
-            if(visited[num] != 0){
-                visited[num] = -1;
-            }
-        }
-        
-        for(int num :nums2){
-            if(visited[num] != 0){
-                visited[num] = -1;
-            }
-        }
-        
-        for(int i = 0; i < 1001; i++){
-            if(visited[i] == 0){
-                ans.add(i);
-            }
-        }
         
         return ans.stream().mapToInt(i -> i).toArray();
     }
