85d37f193e6e1a9eed9c1dccc8a9440b1113a6ae
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-25 21:12:37 +0300

    Time: 5 ms (60.98%), Space: 44.9 MB (7.85%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -4,40 +4,19 @@ class Solution {
         HashMap<Integer,Integer> map = new HashMap<>();
         int [] ans = new int[nums1.length];
         
-        for (int i = 0; i  < nums1.length; ++i)
-        {
-            map.put(nums1[i],i);
-        }
-        
         for(int j = 0; j < nums2.length; ++j)
         {
-            if (stk.empty() || stk.peek() > nums2[j])
-            {
-                stk.push(nums2[j]);
-            }
-            else if (stk.peek() < nums2[j])
-            {
                 while (!stk.empty() && stk.peek() < nums2[j])
                 {
-                    int top = stk.pop();
-                    if (map.containsKey(top))
-                    {
-                        ans[map.get(top)]  = nums2[j];
-                    }
+                    map.put(stk.pop(),nums2[j]);
                 }
                 stk.push(nums2[j]);
-            }
         }
         
-        while (!stk.empty())
-        {
-            int top = stk.pop();
-        
-            if (map.containsKey(top))
-            {
-                ans[map.get(top)] = -1;
-            }
-        }
+       for (int i = 0; i < nums1.length; ++i)
+       {
+           ans[i] = map.getOrDefault(nums1[i],-1);
+       }
         return ans;
     }
 }
