13c9a0f48403f24b1235de219ec738fce87c85a5
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-31 14:05:51 +0300

    Time: 66 ms (47.24%), Space: 17 MB (14.73%) - LeetHub

--- 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.py
@@ -1,24 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def findBottomLeftValue(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         def dfs(rt, level):
-            if  rt:
-                if self.max_level < level:
-                    self.ans = rt.val
-                    self.max_level = level
-                left = dfs(rt.left ,level + 1)
-                right = dfs(rt.right, level + 1)
-        self.ans = root.val
-        self.max_level = 0
-        dfs(root,0)
-        return self.ans
+            if not rt:
+                return (rt, level - 1)
+            left = dfs(rt.left, level + 1)
+            right  = dfs(rt.right , level + 1)
+            
+            if  left[1] >= right[1]:
+                if left[1] == level and left[0] == None:
+                    return (rt, level)
+                return left
+            else:
+                if right[1] == level and right[0] == None:
+                    return (rt, level)
+                return right
+        return dfs(root,0)[0].val
+        
+            
         
