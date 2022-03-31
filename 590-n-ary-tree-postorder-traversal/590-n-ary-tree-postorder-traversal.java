4ce7bac115892efc8da4e2a41277f8593412016c
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-03-31 21:55:41 +0300

    Time: 2 ms (51.29%), Space: 46.2 MB (44.41%) - LeetHub

---
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    private ArrayList<Integer> result = new ArrayList<>();
+    private void dfs(Node root){
+        if (root == null){
+            return;
+        }
+        for (Node child : root.children){
+            dfs(child);
+        }
+        result.add(root.val);
+    }
+    public List<Integer> postorder(Node root) {
+        dfs(root);
+        return result;
+    }
+}
