3e9bc91012e800ffe84212308bf92e13a41dd55e
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-01 23:13:15 +0300

    Time: 868 ms (43.92%), Space: 15.8 MB (21.32%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
         def bfs():
-            temp = (-1,-1)
-            dist = 0
+            level = -1
             
             while queue:
                 n = len(queue)
+                level += 1
                 for k in range(n):
                     cur = queue.popleft()
                     for d in direction:
@@ -16,33 +16,24 @@ class Solution:
                             continue
                         queue.append((r, c))
                         seen.add((r, c))
-                        temp = (r , c)
-            return temp
+            return level
         
         m = len(grid)
         seen = set()
         queue = deque()
         in_bound = lambda r, c:  0 <= r < m and 0 <= c < m
         direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        result =  float('inf')
-        temp = []
         
         for i in range(m):
             for j in range(m):
                 if grid[i][j] == 1:
                     seen.add((i, j))
                     queue.append((i, j))
-                    temp.append((i , j))
         
-        d = bfs();
-        if (d[0] == -1 or d[1] == -1):
-            return  -1
+        if len(queue) == 0 or len(queue) == m * m:
+            return -1
         
-        for i in range(len(temp)):
-            t = abs(d[0] - temp[i][0]) + abs(d[1] - temp[i][1])
-            result = min(result, t)
-        
-        return result
+        return bfs()
             
     
     
