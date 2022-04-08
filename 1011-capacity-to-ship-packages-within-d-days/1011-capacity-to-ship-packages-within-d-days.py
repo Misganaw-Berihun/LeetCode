d45ededf152b8f076b1ebf1b63ddf70c496be89d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-08 10:40:35 +0300

    Time: 884 ms (26.38%), Space: 17 MB (81.49%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -3,18 +3,16 @@ class Solution:
         #returns the days required to ship packages if max weight of ship is weight
         def helper(weight):
             n = len(weights)
-            d = 0
+            d = 1
             weight_sum = 0
             for i in range(n):
                 weight_sum += weights[i]
                 if weight_sum > weight:
                     weight_sum = weights[i]
                     d += 1
-            if weights[n - 1] <= weight_sum or weight_sum == weight:
-                d += 1
             return d 
         
-        left, right = max(weights),  sum(weights) + 1
+        left, right = max(weights),  sum(weights) 
         while left < right:
             mid = left + (right - left) // 2
             d = helper(mid)
