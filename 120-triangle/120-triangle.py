7ba7a5920a8e1edd009162c7f7fc41db4fa53bb3
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-08 18:33:00 +0300

    Time: 118 ms (25.48%), Space: 15.2 MB (35.42%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,15 +1,14 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        def dp(i, j):
-            nonlocal memo
-            if (i, j) in memo:
-                return memo[(i,j)]
-            if (i == len(triangle) - 1):
-                return triangle[i][j]
-            
-            memo[(i,j)] = triangle[i][j] + min( dp(i + 1, j) , dp(i + 1, j + 1))
-            return memo[(i,j)]
+        N = len(triangle)
+        ans = []
+        ans.append(triangle[-1])
         
-        memo = {}
-        return dp(0,0)
+        for j in range(N - 2, -1, -1):
+            temp = []
+            for k in range(0,len(ans[-1]) - 1):
+                min_val = triangle[j][k] + min(ans[-1][k] , ans[-1][k + 1])
+                temp.append(min_val)
+            ans.append(temp)
+        return ans[-1][-1]
             
