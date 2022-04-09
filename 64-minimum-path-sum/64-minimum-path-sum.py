a833ded3f9d71fd7f47b515ef09f3970d4f42d81
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-09 11:17:33 +0300

    Time: 120 ms (72.22%), Space: 20 MB (5.78%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -1,19 +1,16 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
+        @cache
         def dp(i, j):
-            nonlocal memo
-            if (i, j) in memo:
-                return memo[(i, j)]
             if i == 0 and j == 0:
                 return grid[i][j]
             
             if i >= len(grid) or i < 0 or j >= len(grid[0]) or j < 0:
                 return float('inf')
             
-            memo[(i, j)] = grid[i][j] + min(dp(i -1, j), dp(i , j - 1))
-            return memo[(i, j)]
+            ans = grid[i][j] + min(dp(i -1, j), dp(i , j - 1))
+            return ans
             
-        memo = {}
         return dp(len(grid) - 1, len(grid[0]) - 1)
             
                 
