65ae2f8587a44a0303a80f613bfb2adc712b906b
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-09 19:02:47 +0300

    Time: 3 ms (34.35%), Space: 42.4 MB (8.22%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -1,14 +1,22 @@
 class Solution {
     private int total = 0;
-    private static int rec(int s , int e , int [] nums , int turn){
+    private static int rec(int s , int e , int [] nums , int turn, 
+                                            HashMap<Pair<Integer, Integer>, Integer> memo){
+        Pair<Integer, Integer> cur = new Pair(s, e);
+        if (memo.containsKey(cur)){
+            return memo.get(cur);
+        }
         if (s == e){
             return nums[s];
              }
-        int a = turn * nums[s] + rec(s + 1,  e, nums , -turn);
-        int b = turn* nums[e] + rec(s, e - 1, nums, -turn);
-        return turn * Math.max(turn * a, turn * b);
+        int a = turn * nums[s] + rec(s + 1,  e, nums , -turn, memo);
+        int b = turn* nums[e] + rec(s, e - 1, nums, -turn, memo);
+        memo.put(cur,turn * Math.max(turn * a, turn * b));
+        return memo.get(cur);
     }
     public boolean PredictTheWinner(int[] nums) {
-        return rec(0, nums.length - 1, nums , 1) >= 0;
+        HashMap<Pair<Integer, Integer>, Integer> memo = 
+                new HashMap<Pair<Integer, Integer>, Integer>();
+        return rec(0, nums.length - 1, nums , 1, memo) >= 0;
     }
 }
