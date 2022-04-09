16157e020769e31286cf26d3d842ba5021a47e92
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-09 10:04:47 +0300

    Time: 1420 ms (45.64%), Space: 25.6 MB (8.59%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -3,12 +3,10 @@ class Solution:
         n = len(prices)
         temp = [0 for i in range(n)]
         temp[n - 1] = prices[n- 1]
-        ans = 0
+        ans = temp[n - 1] - prices[n - 1]
         
         for i in range(n - 2,  -1, -1):
             temp[i] = max(temp[i + 1] , prices[i])
-            
-        for i in range(n):
             ans = max(ans, temp[i] - prices[i])
             
         return ans
