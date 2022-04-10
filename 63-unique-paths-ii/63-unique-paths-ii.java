a2f2266e2aa0626d570b4266bc1450564f4b8f20
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-10 12:48:40 +0300

    Time: 1 ms (53.84%), Space: 42.7 MB (26.03%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,26 +1,22 @@
-class Solution {
-    private static int dp(int [][]obstacleGrid, int r, int c, HashMap<String, Integer> memo){
-        String key = r + "," + c;
-        if (memo.containsKey(key)) return memo.get(key);
-        if (r == 0 && c == 0) return 1;
-        if (r < 0 || c < 0) return 0;
-        int a = 0, b = 0;
-        if ( r >= 1 && obstacleGrid[r - 1][c] == 0){
-            a = dp(obstacleGrid, r - 1, c, memo);
-        }
-        if ( c >= 1 && obstacleGrid[r][c - 1] == 0){
-            b = dp(obstacleGrid, r, c - 1, memo);
-        }
-        memo.put(key, a + b);
-        return memo.get(key);
-    }
-    
+class Solution {    
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int n = obstacleGrid.length, m = obstacleGrid[0].length;
-        HashMap<String, Integer> memo = new HashMap<String, Integer>();
-        if (obstacleGrid[0][0] == 1|| obstacleGrid[n - 1][m - 1] == 1 ){
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[n - 1][m - 1] == 1){
             return 0;
         }
-        return dp(obstacleGrid, n - 1 , m - 1, memo);
+        int [][] dp = new int[n + 1][m + 1];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if (i == 0 && j == 0) {
+                    dp[i + 1][j + 1] = 1;
+                }else if(obstacleGrid[i][j] == 1){
+                    dp[i + 1][j + 1] = 0;
+                }
+                else{
+                    dp[i + 1][j + 1] = dp[i + 1][j] + dp[i ][j + 1];
+                } 
+            }
+        }
+        return dp[n][m];
     }
 }
