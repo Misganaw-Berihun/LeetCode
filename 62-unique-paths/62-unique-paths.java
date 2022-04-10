faa3e52e9ebf6ef9e06c38e0620c6cae4dba5576
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-10 08:29:14 +0300

    Time: 0 ms (100.00%), Space: 39.1 MB (88.93%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -2,13 +2,26 @@ class Solution {
     private static int dp(int m, int n , int [][]memo){
         if (memo[m][n] > 0){ return memo[m][n]; }
         if (m == 0 || n == 0){ return 0; }
-        if (m == n && n == 1){ return 1; }
+        if (m == 1 && n == 1){ return 1; }
         
         memo[m][n ] = dp(m - 1, n, memo) + dp(m, n - 1, memo);
         return memo[m][n];
     }
     public int uniquePaths(int m, int n) {
-        int [][]memo = new int[m + 1][n + 1];
-        return dp(m, n , memo);
+        int dp[][] = new int[m + 1][n + 1];
+        
+        for (int i = 0; i <= m; i++){
+            for(int j = 0; j <= n; j++){
+                if (i == 1 && j == 1){
+                    dp[i][j] = 1;
+                }
+                else if (i == 0 || j ==0){
+                    dp[i][j] = 0;
+                }else{
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return   dp[m][n];     
     }
 }
