7dae8ad4b1b2308c75f200a00670865de48263cc
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-17 08:09:58 +0300

    Time: 1081 ms (74.94%), Space: 33.8 MB (11.35%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -3,22 +3,16 @@ class Solution:
         n = len(security)
         inc_cnt = [0 for i in range(n)]
         dec_cnt = [0 for i in range(n)]
-        k = 0
         ans = []
         
         for i in range(1,n):
             if security[i] <= security[i - 1]:
-                dec_cnt[i] = i - k
-            elif security[i] > security[i - 1]:
-                k = i
-          
-        k = n - 1
+                dec_cnt[i] = 1 + dec_cnt[i - 1]
+        
         for j in range(n - 2, -1, -1):
             if security[j] <= security[j + 1]:    
-                inc_cnt[j] = k - j
-            elif security[j] > security[j + 1]:
-                k = j
-    
+                inc_cnt[j] = inc_cnt[j + 1] + 1 
+      
         for i in range(n):
             if inc_cnt[i] >= time and dec_cnt[i] >= time:
                 ans.append(i)
