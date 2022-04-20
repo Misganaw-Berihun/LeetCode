f4f2debdc6dcc847864f831dd27fe3a2dbc4de75
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-20 12:49:01 +0300

    Time: 258 ms (17.39%), Space: 14.7 MB (73.72%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,11 +1,12 @@
 class UnionFind:
-    def __init__(self, max_row, max_col):
+    def __init__(self, max_row, max_col, grid):
         self.parent = {}
         self.rank = {}
         for i in range(max_row):
             for j in range(max_col):
-                self.parent[(i,j)] = (i, j)
-                self.rank[(i, j)] = 0
+                if grid[i][j] == 1:
+                    self.parent[(i,j)] = (i, j)
+                    self.rank[(i, j)] = 0
     
     def find(self, v):
         if self.parent[v] == v:
@@ -24,12 +25,12 @@ class UnionFind:
             if self.rank[par_a] == self.rank[par_b]:
                 self.rank[par_b] += 1
                 
-    def findAns(self):
-        return max(self.cnt.values()) + 1
+    def getParent(self):
+        return self.parent
             
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        uf = UnionFind(len(grid), len(grid[0]))
+        uf = UnionFind(len(grid), len(grid[0]), grid)
         direction = [(-1, 0), (1, 0), (0, 1), (0, -1)]
         inbound = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
         ans = defaultdict(int)
@@ -41,15 +42,11 @@ class Solution:
                     for d in direction:
                         r = i + d[0]
                         c = j + d[1]
-                        
                         if inbound(r, c) and grid[r][c] == 1:
                             uf.union((i, j), (r, c))
         
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    ans[uf.find((i,j))] += 1
-                    ret = max(ret, ans[uf.find((i, j))])
-    
+        for k in uf.getParent():
+            ans[uf.find(k)] += 1
+            ret = max(ret, ans[uf.find(k)])
         return ret
                 
