9a24706598e004d0e484c89d4dc8b48c6b4c562f
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-25 21:45:49 +0300

    Time: 2668 ms (5.03%), Space: 256.1 MB (5.54%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,15 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        def dp(i, prev, memo = {}):
+        @cache
+        def dp(i, prev):
             if i >= len(prices):
                 return 0
-            if (i, prev) in memo:
-                return memo[(i, prev)]
             if prev == 'B':
-                memo[(i, prev)] =  max(prices[i] + dp(i + 1, 'S') - fee, dp(i + 1, 'B'))
+                return max(prices[i] + dp(i + 1, 'S') - fee, dp(i + 1, 'B'))
             elif prev == 'S':
-                memo[(i, prev)] = max(-prices[i] + dp(i + 1, 'B') , dp(i + 1, 'S'))
-            return memo[(i, prev)]
+                return max(-prices[i] + dp(i + 1, 'B') , dp(i + 1, 'S'))
             
         return dp(0, "S");
             
