bf9cfcc617419960cc0fcfce765e33be1a966fcb
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-04-26 21:08:12 +0300

    Time: 1460 ms (49.78%), Space: 19.9 MB (5.43%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,17 +1,16 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        @cache
         def dfs(course, target):
-            nonlocal adjacencyList
-            if (course, target) in memo:
-                return (course, target)
-            
+            nonlocal adjacencyList, memo
             if course == target:
                 return True
             
+            if (course, target) in memo:
+                return memo[(course, target)]
+            
             for cor in adjacencyList[course]:
                 if dfs(cor, target):
-                    memo[(course, target)] =  True
+                    memo[(course, target)] = True
                     return True
             memo[(course, target)] = False
             return False
