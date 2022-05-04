1e51618ad742e0d07a100c456cbe103351a4f21d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-04 18:47:59 +0300

    Time: 8928 ms (89.93%), Space: 78.3 MB (23.71%) - LeetHub

--- 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -2,7 +2,9 @@ class TrieNode:
     def __init__(self):
         self.edges = {}
         self.is_word = False
+        
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
         
@@ -13,23 +15,24 @@ class WordDictionary:
                 cur.edges[w] = TrieNode()
             cur = cur.edges[w]
         cur.is_word = True
-
+        
     def search(self, word: str) -> bool:
-        def dfs(i, cur):
+        def dfs(i, curNode):
             if i == len(word):
-                if cur.is_word:
-                    return True
-                return False
-            if word[i] in cur.edges:
-                if dfs(i + 1, cur.edges[word[i]]):
+                return curNode.is_word
+            
+            if word[i] in curNode.edges:
+                if dfs(i + 1, curNode.edges[word[i]]):
                     return True
             elif word[i] == '.':
-                for neig in cur.edges:
-                    if dfs(i + 1, cur.edges[neig]):
+                for neig in curNode.edges:
+                    if (dfs(i + 1, curNode.edges[neig])):
                         return True
             return False
-        return dfs(0, self.root)        
         
+        return dfs(0, self.root) 
+                    
+
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
