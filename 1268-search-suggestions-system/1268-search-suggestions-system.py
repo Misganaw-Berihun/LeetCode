ef6f7a9547e1faa0689848a587f23035145b2f25
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-05 22:23:18 +0300

    Time: 302 ms (47.14%), Space: 19 MB (43.45%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self.prefixes = defaultdict(list)
+    
+    def insert(self, word):
+        cur = self.root
+        pre = ""
+        for w in word:
+            pre += w
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            self.prefixes[pre].append(word)
+        cur = cur.children[w]
+        cur.is_word = True
+    
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        node = Trie()
+        t = ""
+        ans = []
+        for product in products:
+            node.insert(product)
+        
+        for let in searchWord:
+            t += let
+            temp = sorted(node.prefixes[t])
+            if len(temp) < 3:
+                ans.append(temp)
+            else:
+                ans.append(temp[:3])
+                
+        return ans
+            
+            
+        
+            
+        
