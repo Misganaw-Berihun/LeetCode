fa66161671de0178e710a67fec55a516607ad35e
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-09 23:21:49 +0300

    Time: 131 ms (21.12%), Space: 18.1 MB (5.29%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -4,11 +4,14 @@ class Solution:
         timePoints.sort()
         zipped = list(zip(timePoints,timePoints[1:] + [timePoints[0]]))
         for t1, t2 in zipped:
-            min1 = 60 * int(t1[:2]) + int(t1[3:])
-            min2 = 60 * int(t2[:2]) + int(t2[3:])
+            a, b = t1.split(":")
+            c, d = t2.split(":")
+            min1 = 60 * int(a) + int(b)
+            min2 = 60 * int(c) + int(d)
             ans = min(abs(min1 - min2), ans)
-        l = zipped[-1]
-        ans = min(ans, abs((60 * (24 + int(l[1][:2])) + int(l[1][3:])) - (60 * int(l[0][:2]) + int(l[0][3:])) ))
+        a, b = zipped[-1][1].split(':')
+        c, d = zipped[-1][0].split(':')
+        ans = min(ans, abs((60 * (24 + int(a)) + int(b)) - (60 * int(c) + int(d)) ))
         return ans
             
         
