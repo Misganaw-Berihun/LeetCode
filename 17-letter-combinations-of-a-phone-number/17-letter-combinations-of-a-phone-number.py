1569efefd637038510a6bd3b29ba7ecb8b17b66c
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-10 18:21:23 +0300

    Time: 48 ms (34.43%), Space: 14 MB (31.71%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        references = {
+                            "2" : ["a", "b", "c"],
+                            "3": ["d", "e", "f"],
+                            "4": ["g", "h", "i"],
+                            "5": ["j", "k", "l"],
+                            "6": ["m","n","o"],
+                            "7": ["p","q","r","s"],
+                            "8": ["t", "u", "v"],
+                            "9": ["w", "x", "y","z"]
+                        }
+        ans = []
+        def findComb(i, s):
+            if i < 0:
+                ans.append(''.join(s))
+                return 
+            for ch in references[digits[i]]:
+                s[i] = ch
+                findComb(i - 1, s)
+        
+        if digits == "":
+            return []
+        temp = [0 for i in range(len(digits))]
+        findComb(len(digits) - 1, temp)
+        return ans
+        
