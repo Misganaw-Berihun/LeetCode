0366fd518dd8b6aa03f9ae203174e43d2e1e6c78
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-13 17:24:18 +0300

    Time: 56 ms (28.34%), Space: 14 MB (25.19%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -4,27 +4,19 @@ class Solution:
         i, j = n - 1, m - 1
         ans = []
     
-        while i >= 0 and j >= 0:
-            d, e = int(a[i]), int(b[j])
-            res = c ^ (e ^ d)
+        while i >= 0 or j >= 0:
+            d, e = '0', '0'
+            if i >= 0:
+                d = a[i]
+            if j >= 0:
+                e = b[j]
+            d , e = int(d), int(e)
+            res = d ^ e ^ c
             c = (e & d) | (c & e) | (c & d)
             ans.append(str(res ))
             i -= 1
             j -= 1
         
-        while i  >= 0:
-            d = int(a[i])
-            res = d ^ c
-            c = (c & d)
-            ans.append(str(res))
-            i -= 1
-        
-        while j >= 0:
-            e = int(b[j])
-            res = e ^ c
-            c = (c & e)
-            ans.append(str(res))
-            j -= 1
         if c == 1:
             ans.append(str(c))
         return ''.join(ans[::-1])
