1acbeccf90ea6d90b3f0143888a5a9519793777d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-13 21:01:15 +0300

    Time: 626 ms (5.13%), Space: 25 MB (89.21%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -1,17 +1,38 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        def bitMask(temp):
+            mask = 0
+            
+            for i in range(10):
+                if temp[i] == 'A':
+                    mask |= 1 << i
+                    
+            for i in range(10, 20):
+                if temp[i % 10] == 'C':
+                    mask |= 1 <<i
+            
+            for i in range(20, 30):
+                if temp[i % 10] == 'G':
+                    mask |= 1 << i
+            
+            for i in range(30, 40):
+                if temp[i % 10] == 'T':
+                    mask |= 1 << i
+                    
+            return mask
         occured = dict()
         ans = []
         
         for i in range(len(s) - 9):
             temp = s[i: i + 10]
-            seen_before = occured.get(temp)
+            mask = bitMask(temp)
+            seen_before = occured.get(mask)
             if seen_before == None:
-                occured[temp] = 1
+                occured[mask] = 1
             else:
-                if occured[temp] != -1:
+                if occured[mask] != -1:
                     ans.append(temp)
-                    occured[temp] = -1
+                    occured[mask] = -1
                 
         return ans
         
