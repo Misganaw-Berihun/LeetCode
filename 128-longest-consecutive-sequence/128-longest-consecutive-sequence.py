6030a2be25f2499081c4fe335475009135c4c9d6
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-17 18:33:38 +0300

    Time: 2544 ms (21.18%), Space: 28.6 MB (21.04%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,51 +1,15 @@
-class UnionFind:
-    def __init__(self):
-        self.parent = {}
-        self.size = {}
-        self.max_size = 0
-        
-    def makeSet(self, val):
-        self.parent[val] = val
-        self.size[val] = 1
-        self.max_size = 1
-        
-    def find(self, v):
-        if self.parent[v] == v:
-            return v
-        self.parent[v] = self.find(self.parent[v])
-        return self.parent[v]
-    
-    def union(self, u, v):
-        u = self.find(u)
-        v = self.find(v)
-        
-        if u != v:
-            if self.size[u] < self.size[v]:
-                u, v = v, u
-            self.parent[v] = u
-            self.size[u] += self.size[v]
-            self.max_size = max(self.max_size, self.size[u])
-    
-    def get_max_size(self):
-        return self.max_size
-        
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        occur = set(nums)
-        n = len(nums)
-        uf = UnionFind()
-        
-        for i in range(n):
-            uf.makeSet(nums[i])
-        
-        for i in range(n):
-            if (nums[i] - 1) in occur:
-                uf.union(nums[i] - 1, nums[i])
-            if (nums[i] + 1) in occur:
-                uf.union(nums[i] + 1, nums[i])
-        return uf.get_max_size()
-      
-    
-        
-        
-        
+        occur, cur_streak, longest_streak = set(nums), 0, 0
+        for num in nums:
+            if num - 1 not in occur:
+                cur = num
+                cur_streak = 1
+                
+                while cur + 1 in occur:
+                    cur = cur + 1
+                    cur_streak += 1
+                    
+                longest_streak = max(longest_streak, cur_streak)
+                
+        return longest_streak        
