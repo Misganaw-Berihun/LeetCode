3832bd178cd110a4ec5f87897498aa465d1c4e89
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-17 16:29:44 +0300

    Time: 796 ms (38.59%), Space: 45 MB (5.07%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -0,0 +1,52 @@
+class UnionFind:
+    def __init__(self):
+        self.parent = {}
+        self.size = {}
+        self.max_size = 1
+        
+    def makeSet(self, val):
+        self.parent[val] = val
+        self.size[val] = 1
+        
+    def find(self, v):
+        if self.parent[v] == v:
+            return v
+        self.parent[v] = self.find(self.parent[v])
+        return self.parent[v]
+    
+    def union(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        
+        if u != v:
+            if self.size[u] < self.size[v]:
+                u, v = v, u
+            self.parent[v] = u
+            self.size[u] += self.size[v]
+            self.max_size = max(self.max_size, self.size[u])
+    
+    def get_max_size(self):
+        return self.max_size
+        
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        occur = set(nums)
+        n = len(nums)
+        uf = UnionFind()
+        
+        for i in range(n):
+            uf.makeSet(nums[i])
+        
+        for i in range(n):
+            if (nums[i] - 1) in occur:
+                uf.union(nums[i] - 1, nums[i])
+            if (nums[i] + 1) in occur:
+                uf.union(nums[i] + 1, nums[i])
+        return uf.get_max_size()
+      
+    
+        
+        
+        
