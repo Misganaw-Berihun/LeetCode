2597598a41227622c9943aee08c3e934e6269535
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-25 21:44:29 +0300

    Time: 181 ms (50.41%), Space: 18.4 MB (85.07%) - LeetHub

--- 648-replace-words/648-replace-words.py
+++ 648-replace-words/648-replace-words.py
@@ -1,50 +1,19 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.index = -1
-        
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def insert(self, word, idx):
-        cur = self.root
-        for w in word:
-            if w not in cur.children:
-                cur.children[w] = TrieNode()
-            cur = cur.children[w]
-        cur.index = idx
-        
-    def search_word(self, word):
-        cur = self.root
-        for w in word:
-            if cur.index >= 0:
-                return cur.index
-            if w not in cur.children:
-                return -1
-            cur = cur.children[w]
-        return -1
-    
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        words  = sentence.split()
         ans = ""
-        node = Trie()
-        idx = 0
-        
-        for word in dictionary:
-            node.insert(word, idx)
-            idx += 1
-        
-        for word in sentence.split():
-            i = node.search_word(word)
-            if len(ans) != 0:
+        dictionary.sort(reverse = True)
+        for i in range(len(words)):
+            word = words[i] 
+            tmp = word
+            for root in dictionary:
+                if word.startswith(root):
+                    tmp = root
+            
+            ans += tmp
+            if i != len(words) - 1:
                 ans += " "
-            if i == -1:
-                ans += word
-            else:
-                ans += dictionary[i]
-        
         return ans
-                
+        
         
         
