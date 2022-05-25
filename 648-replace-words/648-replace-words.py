a3ed6d9487115e5f58e03b0aee14a7c094e86ff5
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-25 22:23:57 +0300

    Time: 178 ms (51.58%), Space: 35.8 MB (39.55%) - LeetHub

--- 648-replace-words/648-replace-words.py
+++ 648-replace-words/648-replace-words.py
@@ -1,19 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isword = False
+    
 class Solution:
-    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        words  = sentence.split()
-        ans = ""
-        dictionary.sort(reverse = True)
-        for i in range(len(words)):
-            word = words[i] 
-            tmp = word
-            for root in dictionary:
-                if word.startswith(root):
-                    tmp = root
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        cur = self.root
+        for w in word:
+            if w not in cur.children:
+                cur.children[w] = TrieNode()
+            cur = cur.children[w]
+        cur.isword = True
+        
+    def replace(self, word):
+        cur, rep = self.root, []
+        for w in word:
+            if w not in cur.children:
+                return None
+            rep.append(w)
+            cur = cur.children[w]
+            if cur.isword:
+                return ''.join(rep)
             
-            ans += tmp
-            if i != len(words) - 1:
-                ans += " "
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        words = sentence.split()
+        ans, n = "", len(words)
+        for root in dictionary:
+            self.insert(root)
+        
+        for i in range(n):
+            word = words[i]
+            rep = self.replace(word)
+            
+            if rep:
+                ans += rep
+            else:
+                ans += word
+                
+            if i != n - 1:
+                ans += ' '
+                
         return ans
+        
+        
         
         
         
