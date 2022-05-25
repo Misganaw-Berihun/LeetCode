bc5257d8063c0c06cd248cf095b246e04333d09c
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-05-25 20:54:56 +0300

    Time: 51 ms (36.80%), Space: 14 MB (72.62%) - LeetHub

--- 677-map-sum-pairs/677-map-sum-pairs.py
+++ 677-map-sum-pairs/677-map-sum-pairs.py
@@ -1,52 +1,19 @@
-class TrieNode:
-    def  __init__(self):
-        self.children = {}
-        self.value = 0
-        self.isword = False
-        
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        self.summ = 0
-        
-    def insert(self, word, val):
-        cur = self.root
-        for w in word:
-            if w not in cur.children:
-                cur.children[w] = TrieNode()
-            cur = cur.children[w]
-        cur.isword = True
-        cur.value = val
-        
-    def findPrefix(self, prefix):
-        cur = self.root
-        for let in prefix:
-            if let not in cur.children:
-                return None
-            cur = cur.children[let]
-        return cur
-    
-    def dfs(self, start):
-        if not start:
-            return
-        if start.isword:
-            self.summ += start.value 
-        for child in start.children:
-            self.dfs(start.children[child])
-        
 class MapSum:
 
     def __init__(self):
-        self.theTrie = Trie()
+        self.map = {}
+        
 
     def insert(self, key: str, val: int) -> None:
-        self.theTrie.insert(key, val)
+        self.map[key] = val
         
 
     def sum(self, prefix: str) -> int:
-        self.theTrie.summ = 0
-        self.theTrie.dfs(self.theTrie.findPrefix(prefix))
-        return self.theTrie.summ
+        ret = 0
+        for k in self.map.keys():
+            if k.startswith(prefix):
+                ret += self.map[k]
+        return ret
         
 
 
