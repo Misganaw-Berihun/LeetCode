1e7fd2ef48dcc6d8fd2703d13451135d1c43b081
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-02 23:49:07 +0300

    Time: 49 ms (35.47%), Space: 14 MB (16.14%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,9 +1,23 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def dfs(src, dest):
+            nonlocal visited
+            if src not in adjList:
+                return -1 
+            
+            if src == dest:
+                return 1
+            visited.add(src)
+            for nei in adjList[src]:
+                if nei not in visited:
+                    tmp = dfs(nei, dest)
+                    if  tmp and tmp == -1:
+                        continue
+                    return div[(src, nei)] * tmp
+            return -1
         adjList = defaultdict(list)
         div = {}
         ret = [-1 for i in range(len(queries))]
-        
         for i in range(len(equations)):
             a, b = equations[i]
             adjList[a].append(b)
@@ -12,25 +26,9 @@ class Solution:
             div[(b, a)] = 1 / values[i]
         
         for j in range(len(queries)):
-            x, y = queries[j]
-            queue = deque()
             visited = set()
-            queue.append((x, 1))
-            visited.add(x)
-        
-            while (queue):
-                cur = queue.popleft()
-                if cur[0] not in adjList:
-                    ret[j] = -1
-                    continue
-                if cur[0] == y:
-                    ret[j] = cur[1]
-                    break
-                    
-                for child in adjList[cur[0]]:
-                    if child not in visited:
-                        queue.append((child, cur[1] * div[(cur[0],  child)]))
-                        visited.add(child)
+            x, y = queries[j]
+            ret[j] = dfs(x, y)
         return ret
                 
         
