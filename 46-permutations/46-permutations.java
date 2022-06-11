134a13d309de9370cef6de417174bdd177234717
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-11 08:47:45 +0300

    Time: 2 ms (67.63%), Space: 44.4 MB (63.93%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,27 +1,20 @@
 class Solution {
-    private static void swap(int i, int j, int [] nums){
-        int  temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    private static void findPermutation(int i , int[] nums, List<List<Integer>> ans){
-        if (i >= nums.length - 1){
-            ArrayList<Integer> temp = new ArrayList();
-            for (int k = 0; k < nums.length; k++){
-                temp.add(nums[k]);
-            }
-            ans.add(temp);
-            return;
-        }
-        for (int j = i; j < nums.length; j++){
-            swap(i, j, nums);
-            findPermutation(i + 1, nums, ans);
-            swap(i, j, nums);
-        }
-    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList();
-        findPermutation(0, nums, ans);
+        List<Integer> temp = new ArrayList();
+        temp.add(nums[0]);
+        ans.add(temp);
+        for (int i = 1; i < nums.length; i++){
+            List<List<Integer>> new_ans = new ArrayList();
+            for (int j = 0; j <= i; j++){
+                for (List<Integer> l: ans){
+                    List<Integer> cur = new ArrayList(l);
+                    cur.add(j, nums[i]);
+                    new_ans.add(cur);
+                }
+            }
+            ans = new_ans;
+        }
         return ans;
     }
 }
