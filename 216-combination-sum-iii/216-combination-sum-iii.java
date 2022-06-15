e297cd27a23a51bfd2e5c886348048f6b4490c94
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-15 17:51:31 +0300

    Time: 2 ms (15.93%), Space: 42.9 MB (6.17%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.java
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -3,23 +3,22 @@ class Solution {
                                                             int n,
                                                             List<Integer> comb,
                                                             List<List<Integer>> result,
-                                                            int cur
+                                                            int cur,
+                                                            int [] candidates
                                                             ){
-        if (n < 0) return;
         if (k == 0 && n == 0){
-            result.add(new ArrayList<Integer>(comb));
+            result.add(comb);
             return;
         }
-        
-        for (int i = cur; i <= 9; i++){
-            comb.add(i);
-            backtrack(k - 1, n - i, new ArrayList<Integer>(comb), result, i + 1);
-            comb.remove(comb.size() - 1);
-        }
+        if (cur >= 9 || n < 0 || k < 0) return;
+        backtrack(k, n, new ArrayList<Integer>(comb), result, cur + 1, candidates);
+        comb.add(candidates[cur]);
+        backtrack(k - 1, n - candidates[cur], new ArrayList<Integer>(comb), result, cur + 1, candidates);
     }
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
-        backtrack(k, n, new ArrayList<Integer>(), res, 1);
+        int [] candidates = {1,2,3,4,5,6,7,8,9};
+        backtrack(k, n, new ArrayList<Integer>(), res, 0, candidates);
         return res;
     }
 }
