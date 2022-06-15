bc75bd692574f09a5f1cf5f747241ba545c8eace
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-15 17:16:06 +0300

    Time: 20 ms (5.43%), Space: 49.3 MB (5.07%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,5 +1,4 @@
 class Solution {
-    private Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
     private void backtrack(int[] candidates, int target, List<Integer> comb, List<List<Integer>>ans, int curIdx){
         if (target < 0) return;
         if (target == 0){
@@ -7,28 +6,17 @@ class Solution {
             return;
         }
         for (int i = curIdx; i < candidates.length; i++){
-            if (counter.get(candidates[i])  == 0){
+            if (i > curIdx && candidates[i] == candidates[ i - 1])
                 continue;
-            }
             comb.add(candidates[i]);
-            counter.put(candidates[i], counter.get(candidates[i]) - 1);
-            backtrack(candidates, target - candidates[i], new ArrayList<Integer>(comb), ans, i);
+            backtrack(candidates, target - candidates[i], new ArrayList<Integer>(comb), ans, i + 1);
             comb.remove(comb.size() - 1);
-            counter.put(candidates[i], counter.get(candidates[i]) + 1);
         }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> ans  = new ArrayList<List<Integer>>();
-        for (int candidate: candidates){
-            counter.putIfAbsent(candidate, 0);
-            counter.put(candidate, counter.get(candidate) + 1);
-        }
-        int [] arr = new int[counter.size()];
-        int i = 0;
-        for(int key: counter.keySet()){
-            arr[i++] = key;
-        }
-        backtrack(arr, target, new ArrayList<Integer>(), ans, 0);
+        Arrays.sort(candidates);
+        backtrack(candidates, target, new ArrayList<Integer>(), ans, 0);
         return ans;
     }
 }
