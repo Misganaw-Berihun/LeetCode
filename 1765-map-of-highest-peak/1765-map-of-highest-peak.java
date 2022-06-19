9f007b7d2af00071474163a87b578dcc440b6bcc
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-19 12:48:56 +0300

    Time: 85 ms (80.53%), Space: 224.9 MB (57.90%) - LeetHub

--- 1765-map-of-highest-peak/1765-map-of-highest-peak.java
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -2,15 +2,17 @@ class Solution {
     public int[][] highestPeak(int[][] isWater) {
         int n = isWater.length, m = isWater[0].length;
         ArrayDeque<int []> queue = new ArrayDeque<int []> ();
-        int [][] visited = new int[n][m];
+        int [][] heights = new int[n][m];
+        for (int i = 0; i < n; i++){
+            Arrays.fill(heights[i], -1);
+        }
         final int [][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         
         for (int i = 0; i < n; i++){
             for (int j  = 0; j < m; j++){
                 if (isWater[i][j] == 1){
                     queue.addLast(new int[]{i, j});
-                    isWater[i][j] = 0;
-                    visited[i][j] = 1;
+                    heights[i][j] = 0;
                 }
             }
         }
@@ -22,14 +24,13 @@ class Solution {
                 
                 for (int [] d : direction){
                     int r = cur[0] + d[0], c = cur[1] + d[1];
-                    if (r < 0 || r >= n || c < 0 || c >=m || visited[r][c] == 1)
+                    if (r < 0 || r >= n || c < 0 || c >=m || heights[r][c] != -1)
                         continue;
-                    isWater[r][c] = isWater[cur[0]][cur[1]] + 1;
+                    heights[r][c] = heights[cur[0]][cur[1]] + 1;
                     queue.addLast(new int[]{r, c});
-                    visited[r][c] = 1;
-                }
+                     }
             }
         }
-        return isWater;
+        return heights;
     }
 }
