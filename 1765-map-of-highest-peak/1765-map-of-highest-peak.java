519e5523478d8a8de0db203954246fdbbb8fb995
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-19 12:41:51 +0300

    Time: 134 ms (63.69%), Space: 259.1 MB (10.53%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int n = isWater.length, m = isWater[0].length;
+        ArrayDeque<int []> queue = new ArrayDeque<int []> ();
+        int [][] visited = new int[n][m];
+        final int [][] direction = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        
+        for (int i = 0; i < n; i++){
+            for (int j  = 0; j < m; j++){
+                if (isWater[i][j] == 1){
+                    queue.addLast(new int[]{i, j});
+                    isWater[i][j] = 0;
+                    visited[i][j] = 1;
+                }
+            }
+        }
+        while (queue.size() > 0){
+            int k = queue.size();
+            
+            for (int i = 0; i < k; i++){
+                int [] cur = queue.pollFirst();
+                
+                for (int [] d : direction){
+                    int r = cur[0] + d[0], c = cur[1] + d[1];
+                    if (r < 0 || r >= n || c < 0 || c >=m || visited[r][c] == 1)
+                        continue;
+                    isWater[r][c] = isWater[cur[0]][cur[1]] + 1;
+                    queue.addLast(new int[]{r, c});
+                    visited[r][c] = 1;
+                }
+            }
+        }
+        return isWater;
+    }
+}
