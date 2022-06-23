e97f9c45ed1121ab05d4ad9b8f81329c5694f826
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-06-23 10:31:36 +0300

    Time: 44 ms (72.96%), Space: 13.8 MB (75.26%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.py
+++ 389-find-the-difference/389-find-the-difference.py
@@ -1,22 +1,10 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        dict = defaultdict(int)
+        ans = 0
         for ch in s:
-            pos = ord(ch) - ord('z') + 25
-            num = 1 << pos
-            dict[num] += 1
-        
+            ans ^= ord(ch)
         for ch in t:
-            pos = ord(ch) - ord('z') + 25
-            temp = 1 << pos
-            if (temp not in dict or dict[temp] == 0):
-                return ch
-            dict[temp] -= 1
+            ans ^= ord(ch)
             
-                
-            
-
-        
-        
-        
+        return chr(ans)
         
