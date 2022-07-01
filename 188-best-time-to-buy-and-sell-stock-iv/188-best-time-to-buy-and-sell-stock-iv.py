b0b5912f9f94b2403f934f7e41d2e18893a040f6
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-01 09:54:26 +0300

    Time: 524 ms (5.78%), Space: 51.8 MB (17.55%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        def dp(i, prev, trans):
+            if (i, prev, trans) in memo:
+                return memo[(i, prev, trans)]
+            if i >= len(prices) or trans == k:
+                return 0
+            ans = 0
+            if prev == 1:
+                ans = max(dp(i + 1, 1, trans), dp(i + 1, 0, trans + 1) + prices[i])
+            else:
+                ans = max(dp(i + 1, 0, trans), dp(i + 1, 1, trans) -  prices[i])
+            memo[(i, prev, trans)] = ans
+            return ans
+        memo = {}
+        return dp(0, 0, 0)
