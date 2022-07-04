b482a5939f4846e3d3bf709e7e87aeec32332f87
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-04 09:52:43 +0300

    Time: 77 ms (46.53%), Space: 44.1 MB (66.32%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,22 +1,16 @@
 class Solution {
-    private static int dp(int [] nums, int curIdx, int [] memo){
-        if (memo[curIdx] != 0) return memo[curIdx];
-        if (curIdx >= nums.length) return 0;
-        int ans = 1;
-        for (int i = curIdx + 1; i < nums.length; i++){
-            if (nums[i] > nums[curIdx]){
-                ans = Math.max(ans, 1 + dp(nums, i, memo));
-            }
-        }
-        memo[curIdx] = ans;
-        return ans;
-    }
     public int lengthOfLIS(int[] nums) {
-        int ans = 1, n = nums.length;
-        int [] memo = new int[n];
-        for (int i = 0; i < nums.length; i++){
-            int ret = dp(nums, i, memo);
-            ans = Math.max(ans, ret);
+        int n = nums.length;
+        int [] dp = new int[n];
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--){
+            dp[i] = 1;
+            for (int j = i + 1; j < n; j++){
+                if (nums[i] < nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
         }
         return ans;
     }
