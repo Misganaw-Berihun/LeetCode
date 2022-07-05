b3d89334b792ab2f1feeb45221c081b73ecfdf0f
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-05 19:12:05 +0300

    Time: 197 ms (60.91%), Space: 14.8 MB (63.24%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -1,18 +1,17 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        dp = [[float('inf') for i in range(n)] for j in range(n)]
-        direction =  [(-1, 0), (-1, 1), (-1, -1)]
-        
-        for i in range(n):
-            dp[0][i] = matrix[0][i]
+        prev = [matrix[0][i] for i in range(n)]
+        cur = prev.copy()
         
         for i in range(1, n):
             for j in range(n):
-                for x, y in direction:
-                    r, c = x + i, y + j
-                    if r < 0 or c < 0 or c >= n or r >= n:
-                        continue
-                    dp[i][j] = min(dp[i][j], dp[r][c] + matrix[i][j])
+                    if  j == 0:
+                        cur[j] = min(prev[j + 1], prev[j]) + matrix[i][j]
+                    elif j == n - 1:
+                        cur[j] = min(prev[j - 1], prev[j]) + matrix[i][j]
+                    else:
+                        cur[j] = min(prev[j - 1], prev[j], prev[j + 1]) + matrix[i][j]
                     
-        return min(dp[n - 1])
+            prev = cur.copy()       
+        return min(cur)
