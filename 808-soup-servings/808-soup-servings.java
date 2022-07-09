c786324704b793e556c05375fcf1a7f19e6a207d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-09 10:18:37 +0300

    Time: 20 ms (22.58%), Space: 42.9 MB (40.32%) - LeetHub

---
+++ 808-soup-servings/808-soup-servings.java
@@ -0,0 +1,24 @@
+class Solution {
+    private static double dp(int A, int B, Map<String, Double> memo){
+        if (A <= 0 && B <= 0) return 0.5;
+        if (A <= 0) return 1;
+        if (B <= 0) return 0;
+        
+        String key = A + ":" + B;
+        if (memo.containsKey(key)) 
+            return memo.get(key);
+        
+        double prob = 0;
+        for (int ml = 25; ml <= 100; ml += 25){
+            prob += 0.25 * dp(A - ml, B + ml - 100, memo);
+        }
+        
+        memo.put(key, prob);
+        return prob;
+    }
+    public double soupServings(int n) {
+        Map<String, Double> memo = new HashMap<String, Double>();
+        if (n >= 5000) return 1;
+        return dp(n, n, memo);
+    }
+}
