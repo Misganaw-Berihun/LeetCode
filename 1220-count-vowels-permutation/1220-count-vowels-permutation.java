780d083b04875dd461682ed5f69378be6d0d2928
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-09 11:36:52 +0300

    Time: 323 ms (6.26%), Space: 56.9 MB (16.85%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,24 +1,34 @@
 class Solution {
     private static long numStrings(int len, char cur, char [] vowels,
                                   Map<Character, Set<Character>> order,
+                                  Map<Character, Integer> charToIdx,
                                   long [][] memo){
             if (len == 0) return 1;
-            if (memo[len][cur - 97] != 0) return memo[len][cur - 97];
+            int idx = charToIdx.get(cur);
+            if (memo[len][idx] != 0) return memo[len][idx];
             long num = 0;
             for (char v: vowels){
                 if (order.get(cur).contains(v)){
-                    num += numStrings(len - 1, v, vowels, order, memo);
+                    num += numStrings(len - 1, v, vowels, order, charToIdx, memo);
                 }
             }
-            memo[len][cur - 97] = num % 1000000007;
-            return memo[len][cur - 97];
+            memo[len][idx] = num % 1000000007;
+            return memo[len][idx];
     }
     
     public int countVowelPermutation(int n) {
         HashMap<Character, Set<Character>> order = 
                     new HashMap<Character, Set<Character>> ();
         char [] vowels = {'a', 'e', 'i', 'o', 'u'};
-        long [][] memo = new long[n + 1][21];
+        long [][] memo = new long[n + 1][5];
+        Map<Character, Integer> charToIdx = 
+                            new HashMap<Character, Integer>();
+        
+        charToIdx.put('a', 0);
+        charToIdx.put('e', 1);
+        charToIdx.put('i', 2);
+        charToIdx.put('o', 3);
+        charToIdx.put('u', 4);
         
         
         order.put('a', new HashSet<Character>(Arrays.asList('e')));
@@ -29,7 +39,7 @@ class Solution {
         
         long ans = 0;
         for (char v: vowels){
-            ans += numStrings(n - 1, v, vowels, order, memo);
+            ans += numStrings(n - 1, v, vowels, order, charToIdx, memo);
         }
         return (int) (ans % 1000000007);
     }
