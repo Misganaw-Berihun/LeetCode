b8577bcf4378b81456fb814fce46be763ad3b607
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-11 16:11:24 +0300

    Time: 9363 ms (5.01%), Space: 14.2 MB (59.69%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -1,21 +1,20 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        def dp(i, one, zero):
-            if i >= len(strs):
-                if one <= n and zero <= m:
-                    return 0
-                return float("-inf")
-            
-            if one > n or zero > m:
-                return float("-inf")
-            
-            if memo[i][zero][one] != -1:
-                return memo[i][zero][one]
-            include = 1 + dp(i + 1, one + strs[i].count("1"), zero + strs[i].count("0"))
-            exclude = dp(i + 1, one, zero)
-            memo[i][zero][one] = max(include, exclude)
-            return memo[i][zero][one]
         length = len(strs)
-        memo = [[[-1 for i in range(n + 1)] for j in range(m + 1)] for k in range(length + 1)]
-        ans =  dp(0, 0, 0)
-        return ans if ans != float("-inf") else 0
+        prev = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        
+        for k in range(1, length + 1):
+            cur = [[0 for i in range(n + 1)] for j in range(m + 1)] 
+            one_cnt = strs[k - 1].count("1")
+            zero_cnt = len(strs[k - 1]) - one_cnt
+            for j in range(m + 1):
+                for i in range(n + 1):
+                        include = float('-inf')
+                        if i - one_cnt >= 0 and j - zero_cnt >= 0:
+                            include = 1 + prev[j - zero_cnt][i - one_cnt]
+                        exclude = prev[j][i]
+                        cur[j][i] = max(include, exclude)
+            prev = cur
+        return cur[m][n]
+    
+        
