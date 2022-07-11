c78804c788833833a80fc2a2bf96c858846a46aa
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-07-11 16:36:36 +0300

    Time: 5123 ms (27.70%), Space: 14.2 MB (59.69%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -1,20 +1,18 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         length = len(strs)
-        prev = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        cur = [[0 for i in range(n + 1)] for j in range(m + 1)]
         
-        for k in range(1, length + 1):
-            cur = [[0 for i in range(n + 1)] for j in range(m + 1)] 
-            one_cnt = strs[k - 1].count("1")
-            zero_cnt = len(strs[k - 1]) - one_cnt
-            for j in range(m + 1):
-                for i in range(n + 1):
+        for k in range(length):
+            one_cnt = strs[k].count("1")
+            zero_cnt = len(strs[k]) - one_cnt
+            for j in range(m, zero_cnt - 1, -1):
+                for i in range(n, one_cnt - 1, -1):
                         include = float('-inf')
                         if i - one_cnt >= 0 and j - zero_cnt >= 0:
-                            include = 1 + prev[j - zero_cnt][i - one_cnt]
-                        exclude = prev[j][i]
+                            include = 1 + cur[j - zero_cnt][i - one_cnt]
+                        exclude = cur[j][i]
                         cur[j][i] = max(include, exclude)
-            prev = cur
         return cur[m][n]
     
         
