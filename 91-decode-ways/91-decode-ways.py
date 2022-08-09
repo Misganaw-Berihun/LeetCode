c1e70cb1a6c3cb03c9248adac79b277eb4957ed2
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-08-09 17:15:06 +0300

    Time: 47 ms (61.35%), Space: 14.1 MB (30.61%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dicti = {}
+        def dfs(i):
+            if i in dicti:
+                return dicti[i]
+            if i == len(s):
+                return 1
+            cur = 0
+            if 1 <= int(s[i]) <= 9:
+                cur += dfs(i+1)
+            if 10 <= int(s[i:i+2]) <= 26:
+                cur += dfs(i+2)
+            dicti[i] = cur
+            return cur
+        return dfs(0)
