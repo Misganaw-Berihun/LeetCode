bab8bbc76197f575b7b116e476fae25f1ea5372b
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-08-11 16:23:30 +0300

    Time: 52 ms (35.97%), Space: 14 MB (33.52%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def dp(cur, path):
+            if cur == s:
+                res.append(path)
+                return
+            
+            if cur != s[:len(cur)]:
+                return 
+            
+            for word in wordDict:
+                dp(cur + word , path + [word])
+            
+        
+        res = []
+        dp("", [])
+        return [' '.join(res[i]) for i in range(len(res))]
+            
