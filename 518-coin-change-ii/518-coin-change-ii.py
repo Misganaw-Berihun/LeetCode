2bf5f11840430c67c42dc58e168e79f6c1dfcd98
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-09-26 20:03:35 +0300

    Time: 903 ms (34.63%), Space: 29.7 MB (36.92%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.py
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -1,20 +1,16 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        def dp (cur, amt_left):
-            if (cur, amt_left) in memo:
-                return memo[(cur, amt_left)]
-            
-            if amt_left < 0:
-                return 0
-            if amt_left == 0:
-                return 1
-            
-            ways = 0
-            for i in range(cur, len(coins)):
-                ways += dp(i, amt_left - coins[i])
-            memo[(cur, amt_left)] = ways
-            return ways
-        memo = {}
-        coins.sort(reverse=True)
-        return dp(0, amount)
+        N = len(coins)
+        dp = [[0 for i in range(amount + 1)] for j in range(N + 1)]
+        for i in range(N+1):
+            dp[i][0] = 1
+        
+        for i in range(1, N + 1):
+            for j in range(1, amount + 1):
+                if j - coins[i-1] >= 0:
+                    dp[i][j] += (dp[i][j-coins[i-1]] + dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+        
+        return dp[N][amount]
         
