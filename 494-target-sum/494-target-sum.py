e5a2a850580a61d9e058120a4e7a3f296c7716a2
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-09-27 13:19:04 +0300

    Time: 1838 ms (5.03%), Space: 14 MB (86.65%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -5,14 +5,16 @@ class Solution:
         N = mx - mn
         M = len(nums)
         inc = (s + 1)
-        dp = [[0 for i in range(N + 1 + max(nums))] for j in range(M+1)]
-        dp[0][inc] = 1
+        dp = [0 for i in range(N + 1 + max(nums))]
+        dp[inc] = 1
         
         for i in range(1, M+1):
+            new_dp = [0 for i in range(N+1 + max(nums))]
             for j in range(mn, mx + 1):
-                dp[i][j + inc] = (dp[i-1][j-nums[i-1] + inc] + 
-                                    dp[i-1][j+nums[i-1] + inc])
-        return dp[M][target + inc]
+                new_dp[j+inc] = (dp[j-nums[i-1] + inc] + 
+                                    dp[j+nums[i-1] + inc])
+            dp = new_dp
+        return dp[target + inc]
         
         
                 
