ea2c966ae5ba421b87814bb64a2d207f7e97a07a
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-08 22:20:59 +0300

    Time: 2441 ms (9.26%), Space: 27.9 MB (94.28%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        prefix = [0 for i in range(n + 1)]
+        
+        for first, last, seats in bookings:
+            prefix[first-1] += seats
+            prefix[last] -= seats
+        
+        for i in range(1, n + 1):
+            prefix[i] += prefix[i-1]
+        
+        return prefix[:-1]
