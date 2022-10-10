efce4c76668010b86b48c2e96fff7213d39af2bb
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-10 15:29:21 +0300

    Time: 377 ms (96.32%), Space: 14.7 MB (95.70%) - LeetHub

--- 752-open-the-lock/752-open-the-lock.py
+++ 752-open-the-lock/752-open-the-lock.py
@@ -1,38 +1,33 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
-        queue = deque()
-        queue.append(list("0000"))
-        visited = set(deadends)
-        if ("0000" in visited):
+        if "0000" in deadends:
             return -1
-        ans = 0
+        visited = set(deadends)
+        begin = set()
+        end = set()
+        numSteps = 0
+        begin.add("0000")
+        end.add(target)
         
-        while queue:
-            N = len(queue)
-            
-            for i in range(N):
-                cur = queue.popleft()
-                
-                if ''.join(cur) == target:
-                    return ans
-
-                
-                for j in range(len(cur)):
-                    new_cur1 = cur.copy()
-                    new_cur2 = cur.copy()
-                    new_cur1[j] = str((int(cur[j]) + 1) % 10)
-                    new_cur2[j] = str((int(cur[j]) - 1) % 10)
-                    joined1 = ''.join(new_cur1)
-                    joined2 = ''.join(new_cur2)
-                    if joined1 not in visited:
-                        queue.append(new_cur1)
-                        visited.add(joined1)
-                    if joined2 not in visited:
-                        queue.append(new_cur2)
-                        visited.add(joined2)
-            ans += 1
+        while begin and end:
+            temp = set()
+            N = len(begin)
+            for j in range(N):
+                for move in list(begin):
+                    if move in end:
+                        return numSteps
+                    if move in visited:
+                        continue
+                    visited.add(move)
+                    for i in range(4):
+                        curCharInc = move[:i] + str((int(move[i]) + 1)  % 10) + move[i+1:]
+                        curCharDec = move[:i] + str((int(move[i]) - 1) % 10) + move[i+1:]
+                        if curCharInc not in visited:
+                            temp.add(curCharInc)
+                        if curCharDec not in visited:
+                            temp.add(curCharDec)
+            numSteps += 1                
+            begin = end.copy()
+            end =  temp.copy()
+        
         return -1
-                        
-                    
-        
-        
