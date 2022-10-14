ab96af8f338185d3171d7c21bd06bc8d761ff472
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-14 23:41:20 +0300

    Time: 1070 ms (67.91%), Space: 20.1 MB (26.97%) - LeetHub

--- 0630-course-schedule-iii/0630-course-schedule-iii.py
+++ 0630-course-schedule-iii/0630-course-schedule-iii.py
@@ -2,17 +2,15 @@ class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         key = lambda a: a[1]
         courses.sort(key = key)
-        ans, curTime = 0, 0
+        curTime = 0
         heap = []
         
         for duration, lastDay in courses:
             curTime += duration
             heappush(heap, -duration)
-            ans += 1
             
             if curTime > lastDay and heap:
                 top = -heappop(heap)
                 curTime -= top
-                ans -= 1
             
-        return ans
+        return len(heap)
