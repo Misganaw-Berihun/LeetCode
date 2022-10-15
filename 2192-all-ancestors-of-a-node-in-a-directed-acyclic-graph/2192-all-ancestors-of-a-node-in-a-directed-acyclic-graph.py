ce86e61cf94d0aaa4883955772cd48e20a5dddc0
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-15 09:02:19 +0300

    Time: 783 ms (96.86%), Space: 46 MB (28.39%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -1,36 +1,26 @@
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        queue = deque()
-        adjList = defaultdict(list)
-        children = defaultdict(list)
-        indegree = defaultdict(int)
         ans = [set() for i in range(n)]
+        adjList = defaultdict(list)
+        indegree = [0 for i in range(n)]
+        queue = deque()
         
-        for i in range(len(edges)):
-            fro, to = edges[i]
-            adjList[to].append(fro)
-            children[fro].append(to)
-            indegree[to] += 1
-        
+        for fro, to in edges:
+            adjList[fro].append(to)
+            indegree[to]+=1
+            
         for i in range(n):
             if indegree[i] == 0:
                 queue.append(i)
         
         while queue:
             cur = queue.popleft()
-            for p in adjList[cur]:
-                ans[cur].add(p)
-                ans[cur].update(ans[p]) 
-            for neighbour in children[cur]:
-                indegree[neighbour] -= 1
-                if indegree[neighbour] == 0:
-                    queue.append(neighbour)
-                    
-        return [sorted(list(s)) for s in ans]
-        
-        
-    
             
-            
-    
+            for next in adjList[cur]:
+                ans[next].add(cur)
+                ans[next].update(ans[cur])
         
+                indegree[next] -= 1
+                if indegree[next] == 0:
+                    queue.append(next)
+        return [sorted(ans[i]) for i in range(n)]
