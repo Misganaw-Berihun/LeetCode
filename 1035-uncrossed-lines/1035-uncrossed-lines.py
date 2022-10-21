bd3ec87d378da76d0e26519250ca626b1849c4d7
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-21 18:04:41 +0300

    Time: 554 ms (35.32%), Space: 13.9 MB (98.21%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -3,13 +3,14 @@ class Solution:
         len_nums1 = len(nums1)
         len_nums2 = len(nums2)
         
-        dp = [[0 for i in range(len_nums1 + 1)] for j in range(len_nums2 + 1)]
+        dp = [0 for i in range(len_nums1 + 1)]
         for i in range(len_nums2 - 1, -1, -1):
+            new_dp = [0 for i in range(len_nums1 + 1)]
             for j in range(len_nums1 - 1, -1, -1):
                 if nums2[i] == nums1[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
+                    new_dp[j] = 1 + dp[j+1]
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        
-        return dp[0][0]
+                    new_dp[j] = max(dp[j], new_dp[j+1])
+            dp = new_dp
+        return dp[0]
         
