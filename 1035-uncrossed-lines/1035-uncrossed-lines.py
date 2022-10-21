220c25452ed37db8a7ab3042279f0e44fb71d926
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-10-21 17:59:54 +0300

    Time: 464 ms (49.61%), Space: 14.3 MB (63.69%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,22 +1,15 @@
 class Solution:
-    def dp(self, nums1, nums2, idx1, idx2, memo):
-        if idx1 >= len(nums1) or idx2 >= len(nums2):
-            return 0
-        
-        if memo[idx1][idx2] != -1:
-            return memo[idx1][idx2]
-        
-        ans = 0
-        if nums1[idx1] == nums2[idx2]:
-            ans = 1 + self.dp(nums1, nums2, idx1 + 1, idx2 + 1, memo)
-        else:
-            inc_nums1 = self.dp(nums1, nums2, idx1 + 1, idx2, memo)
-            inc_nums2 = self.dp(nums1, nums2, idx1, idx2 + 1, memo)
-            ans = max(inc_nums1, inc_nums2)
-        memo[idx1][idx2] = ans
-        return ans
-            
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        memo = [[-1 for i in range(len(nums2) + 1)] for j in range(len(nums1) + 1)]
-        return self.dp(nums1, nums2, 0, 0, memo)
+        len_nums1 = len(nums1)
+        len_nums2 = len(nums2)
+        
+        dp = [[0 for i in range(len_nums1 + 1)] for j in range(len_nums2 + 1)]
+        for i in range(len_nums2 - 1, -1, -1):
+            for j in range(len_nums1 - 1, -1, -1):
+                if nums2[i] == nums1[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        return dp[0][0]
         
