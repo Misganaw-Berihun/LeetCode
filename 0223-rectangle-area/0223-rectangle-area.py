319c1b9b22e3fc88d45e7e34d12559fe80195a6d
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-01 12:22:08 +0300

    Time: 87 ms (70.86%), Space: 14.1 MB (24.02%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.py
@@ -0,0 +1,19 @@
+class Solution:
+    def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
+        max_starting_x = max(ax1, bx1)
+        max_ending_x = min(ax2, bx2)
+        max_starting_y = max(ay1, by1)
+        max_ending_y = min(ay2, by2)
+        
+        area1 = abs(ax2 - ax1) * abs(ay1 - ay2)
+        area2 = abs(bx2 - bx1) * abs(by1 - by2)
+        
+        val1, val2 = 0, 0
+        if max_ending_x > max_starting_x:
+            val1 = max_ending_x - max_starting_x
+        if max_ending_y > max_starting_y:
+            val2 = max_ending_y - max_starting_y
+        rem = val1 * val2
+        ret = area1 + area2 - rem
+        return ret
+        
