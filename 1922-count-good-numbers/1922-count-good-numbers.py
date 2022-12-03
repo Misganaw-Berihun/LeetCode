6405efea1548260055a6126c2590a7f73521a82e
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-03 21:13:16 +0300

    Time: 64 ms (30.59%), Space: 13.8 MB (97.06%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,14 +1,20 @@
 class Solution:
-    def countGoodNumbers(self, n: int) -> int:
-        if n == 1:
-            return 5
-        i = n // 2
-        res = pow(20,i,1000000007)
+    def mul_mod(self, a, b, MOD):
+        return (a * b) % MOD
         
-        if n%2:
-            res = res * 5  % 1000000007
-        
+    def pow_mod(self, a, b, MOD):
+        res = 1
+        while b:
+            if b & 1 == 1:
+                res = self.mul_mod(res, a, MOD)
+            b >>= 1
+            a = self.mul_mod(a, a, MOD)
         return res
+            
+    def countGoodNumbers(self, n: int) -> int:
+        odd_pow = n // 2
+        even_pow = (n + 1) // 2 
+        MOD = 1_000_000_007
         
-        
-        
+        ans = self.mul_mod(self.pow_mod(5, even_pow, MOD), self.pow_mod(4, odd_pow, MOD), MOD)
+        return ans 
