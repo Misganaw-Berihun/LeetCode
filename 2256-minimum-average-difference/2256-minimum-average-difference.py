346b98298913db6c0723afb6946338569ad65662
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-04 11:34:59 +0300

    Time: 2576 ms (45.62%), Space: 24.9 MB (56.92%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.py
+++ 2256-minimum-average-difference/2256-minimum-average-difference.py
@@ -1,10 +1,7 @@
 class Solution:
     def minimumAverageDifference(self, nums: List[int]) -> int:
         size = len(nums)
-        prefix = [0 for i in range(size + 1)]
-        
-        for i in range(size - 1, -1, -1):
-            prefix[i] = prefix[i + 1] + nums[i]
+        tot = sum(nums)
         
         cur = 0
         min_diff = inf
@@ -15,7 +12,7 @@ class Solution:
             first_ets = cur // (i + 1)
             last_ets = 0
             if (size - i - 1) > 0:
-                last_ets = prefix[i+1] // (size - i - 1)
+                last_ets = (tot - cur) // (size - i - 1)
             diff = abs(first_ets - last_ets)
             
             if min_diff > diff:
