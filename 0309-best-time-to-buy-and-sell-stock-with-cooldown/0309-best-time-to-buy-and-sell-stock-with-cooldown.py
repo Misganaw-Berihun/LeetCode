796d29b012512830de583b1141bc80a3c16e0082
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-23 11:55:26 +0300

    Time: 85 ms (49.10%), Space: 20.8 MB (5.80%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        @cache
+        def dp(i, prev, is_bought):
+            nonlocal n
+            ans = 0
+            if i >= n:
+                return ans
+            
+            sell = dp(i + 1, "s", False) + prices[i]
+            buy = dp(i + 1, "b", True) - prices[i]
+            jmp = dp(i + 1, "j", is_bought)
+            
+            if prev == "s":
+                ans = jmp
+            elif prev == "b":
+                ans = max(sell, jmp)
+            elif prev == "j":
+                if is_bought:
+                    ans = max(sell, jmp)
+                else:
+                    ans = max(jmp, buy)
+            return ans
+        
+        return dp(0, "j", False)
+                
+            
+            
+                    
+        
