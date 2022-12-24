b2fdeceef5f76545539bc9c6021a3a7e8bcca929
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-24 21:30:04 +0300

    Time: 34 ms (94.76%), Space: 13.8 MB (99.21%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,22 +1,15 @@
 class Solution:
-    def mul(self, num1, num2):
-        return (num1 * num2) % self.MOD
-
-    def add(self, num1, num2):
-        res = num1 + num2
-        if res >= self.MOD:
-            res -= self.MOD
-        return res
-    
     def numTilings(self, n: int) -> int:
-        self.MOD = 1_000_000_007
         dp = [0 for i in range(n + 1)]
-        dp[0] = dp[1] = 1
+        dp[0] = 1
         
-        for i in range(2, n + 1):
-            dp[i] = self.add(dp[i], self.add(dp[i - 1], dp[i - 2]))
-            for j in range(3, n + 1):
-                if i >= j:
-                    dp[i] = self.add(dp[i], self.mul(dp[i - j], 2))
+        if n >= 1:
+            dp[1] = 1
         
-        return dp[n] 
+        if n >= 2:
+            dp[2] = 2
+        
+        for i in range(3, n + 1):
+            dp[i] = 2 * dp[i-1] + dp[i-3]
+
+        return dp[n] % 1_000_000_007
