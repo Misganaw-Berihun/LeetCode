7a99c1db3a9f349b73f9a28bbfc60e3381d2e7f7
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-24 21:01:17 +0300

    Time: 894 ms (5.56%), Space: 13.9 MB (85.40%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numTilings(self, n: int) -> int:
+        dp = [0 for i in range(n + 1)]
+        dp[0] = dp[1] = 1
+        
+        for i in range(2, n + 1):
+            dp[i] += (dp[i - 1] + dp[i - 2])
+            for j in range(3, n + 1):
+                if i >= j:
+                    dp[i] += (dp[i - j] * 2)
+        
+        return dp[n] % 1_000_000_007
