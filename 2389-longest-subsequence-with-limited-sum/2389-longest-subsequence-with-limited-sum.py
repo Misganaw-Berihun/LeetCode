0a9429d2f33cd93195178f762237770602828b4f
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2022-12-25 21:23:09 +0300

    Time: 132 ms (84.95%), Space: 14.3 MB (40.29%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.py
@@ -1,28 +1,29 @@
 class Solution:
     def binary_search(self, prefix, target):
         left = 0
-        right = len(prefix) - 1
+        right = len(prefix)
         
-        while left <= right:
-            mid = left + (right - left) // 2
+        while left + 1 < right:
+            mid = left + (right - left)//2
             
             if prefix[mid] <= target:
-                left = mid + 1
+                left = mid
             else:
-                right = mid - 1
-        return left - 1
+                right = mid
         
+        return left
         
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
-        prefix = [0]
-        nums.sort()
-        for i in range(len(nums)):
-            prefix.append(prefix[-1] + nums[i])
-            
-        answer = []
+        size = len(nums)
+        q_size = len(queries)
+        prefix = [0 for i in range(size + 1)]
+        ans = [0 for i in range(q_size)]
         
-        for j in range(len(queries)):
-            res = self.binary_search(prefix, queries[j])
-            answer.append(res)
+        nums.sort()
+        for i in range(1, size + 1):
+            prefix[i] = (prefix[i-1] + nums[i - 1])
             
-        return answer
+        for j in range(q_size):
+            ans[j] = self.binary_search(prefix, queries[j])
+            
+        return ans
