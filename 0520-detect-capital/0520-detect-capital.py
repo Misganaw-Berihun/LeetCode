46fbdc0e3539bebd21e6642890722f084a6c04e2
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-01-02 07:46:26 +0300

    Time: 40 ms (67.94%), Space: 13.9 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -5,18 +5,14 @@ class Solution:
         
         for i in range(word_len):
             if word[i].isupper():
-                cnt += 1
-            
-            else:
-                cnt -= 1
-        
+                cnt += 1       
         
         
         correct_usage = False
-        if cnt == (2 - word_len) and word[0].isupper():
+        if cnt == 1 and word[0].isupper():
             correct_usage = True
         
-        elif abs(cnt) == word_len:
+        elif cnt == word_len or word_len - cnt == word_len:
             correct_usage = True
             
         return correct_usage
