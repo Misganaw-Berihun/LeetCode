b7b783249bce91b65159f0fab98824b031dd877c
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-01-03 07:00:14 +0300

    Time: 1171 ms (5.06%), Space: 14.7 MB (61.89%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -3,13 +3,17 @@ class Solution:
         num_words = len(strs)
         len_of_each_word = len(strs[0])
         unsorted_columns = 0
+        inbound = lambda x: x < num_words
         
         for i in range(len_of_each_word):
             correct_order = True
             j = 1
             
-            while j < num_words and correct_order:
-                if (strs[j][i] < strs[j - 1][i]):
+            while inbound(j) and correct_order:
+                cur_char = strs[j][i]
+                prev_char = strs[j - 1][i]
+                
+                if (cur_char < prev_char):
                     correct_order = False
                 
                 j += 1
