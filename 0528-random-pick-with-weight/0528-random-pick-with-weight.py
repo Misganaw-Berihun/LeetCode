8cbde4ec76f3f2332e5dadb7f7cf5a9a1815f3da
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-02-04 17:19:35 +0300

    Time: 261 ms (56.77%), Space: 18.7 MB (41.52%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.py
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -1,30 +1,29 @@
 class Solution:
 
     def __init__(self, w: List[int]):
-        self.total_sum = sum(w)
-        self.size = len(w)
-        self.prefix = [0 for i in range(self.size + 1)]
-        for i in range(1, self.size + 1):
-            self.prefix[i] = self.prefix[i-1] + w[i-1]
+        self.prefix = w.copy()
+        for i in range(1, len(w)):
+            self.prefix[i] += self.prefix[i-1]
+        self.minn = 1
+        self.maxx = self.prefix[len(w) - 1]
         
-    def binary_search(self, target):
-        left = 1
-        right = self.size
-        
-        while left < right:
-            mid = left + (right - left) // 2
-            
-            if self.prefix[mid] < target:
-                left = mid + 1
-            else:
-                right = mid
-                 
-        return left - 1
-        
+
     def pickIndex(self) -> int:
-        rand = randint(1, self.total_sum)
-        return self.binary_search(rand)
+        l = -1
+        r = len(self.prefix)
+        rand = randint(self.minn, self.maxx)
         
+        while l + 1 < r:
+            m = (l + r) // 2
+            
+            if self.prefix[m] < rand:
+                l = m
+            else:
+                r = m
+        
+        return r
+
+
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
