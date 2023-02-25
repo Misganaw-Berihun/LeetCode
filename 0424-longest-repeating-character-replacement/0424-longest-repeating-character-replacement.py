08a8101507455ca1469481b9c639e81724999e99
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-02-25 20:39:39 +0300

    Time: 106 ms (91.91%), Space: 13.9 MB (51.34%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,23 +1,17 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         countOf = defaultdict(int)
-        ans = 0
-        left = 0
-        mxCnt, freqChar = 0, 0
         strLen = len(s)
         
+        left = 0
         for right in range(strLen):
             countOf[s[right]] += 1
-            if countOf[s[right]] > mxCnt:
-                mxCnt = countOf[s[right]]
-                freqChar = s[right]
-            curLen = right - left + 1
-            charAtLeft = s[left]
-            while left < right and curLen - mxCnt > k:
-                countOf[charAtLeft] -= 1
+            max_val = max(countOf.values())
+            
+            if right - left + 1 - max_val > k:
+                countOf[s[left]] -= 1
                 left += 1
-                charAtLeft = s[left]
-                curLen = right - left + 1
-            ans = max(ans, curLen)
         
-        return ans                
+        return strLen - left
+        
+                       
