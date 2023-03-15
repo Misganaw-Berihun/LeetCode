5e1e0bf90e9ad94e7048758c510e94945b620273
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-03-15 15:18:35 +0300

    Time: 404 ms (28.37%), Space: 32.2 MB (11.27%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,38 +1,21 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        size = len(nums)
-        prev_smaller = [inf for i in range(size)]
-        prev_greater = [inf for i in range(size)]
-        dec_stk = []
-        inc_stk = []
+        n = len(nums)
+        stack = []
+        mins = [0 for i in range(n)]
+        mins[0] = nums[0]
         
-        for i in range(size):
-            while dec_stk and nums[dec_stk[-1]] <= nums[i]:
-                dec_stk.pop()
+        for i in range(1, n):
+            mins[i] = min(nums[i], mins[i-1])
+        
+        for i in range(n):
+            while stack and nums[stack[-1]] <= nums[i]:
+                stack.pop()
             
-            while inc_stk and nums[inc_stk[-1]] >= nums[i]:
-                inc_stk.pop()
-                
-            if dec_stk:
-                prev_greater[i] = dec_stk[-1]
-                
-            if inc_stk:
-                prev_smaller[i] = inc_stk[-1]
-            
-            dec_stk.append(i)
-            inc_stk.append(i)
+            if stack:
+                top = stack[-1]
+                if top > 0 and mins[top - 1] < nums[i]:
+                    return True
+            stack.append(i)
         
-        pattern_exists = False
-        index = 0
-        while index < size and not pattern_exists:
-            greater = prev_greater[index]
-            smaller = inf
-            if (greater != inf):
-                smaller = prev_smaller[greater]
-                while smaller != inf and (nums[smaller] >= nums[index]):
-                    smaller = prev_smaller[smaller]
-
-            pattern_exists = (smaller != inf and (nums[smaller] < nums[index]))
-            index += 1
-        
-        return pattern_exists
+        return False
