d958ca1227649e6df443fb88aabe1eb67425472a
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-03-17 10:34:57 +0300

    Time: 45 ms (33.08%), Space: 14.2 MB (40.13%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,17 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        N = len(nums)
-        nums.sort()
+        def bk(i, path):
+            if len(nums) == i:
+                ans.append(path.copy())
+                return
+            
+            bk(i + 1, path + [nums[i]])
+            while i < len(nums) - 1 and nums[i] == nums[i+1]:
+                i += 1
+            
+            bk(i + 1, path)
         
-        ans = [[]]
-        visited = set()
-        for i in range(N):
-            l = len(ans)
-            for j in range(l):
-                t = ans[j] + [nums[i]]
-                tup = tuple(t)
-                if tup not in visited:
-                    ans.append(t)
-                    visited.add(tup)
+        ans = []
+        nums.sort()
+        bk(0, [])
         return ans
         
