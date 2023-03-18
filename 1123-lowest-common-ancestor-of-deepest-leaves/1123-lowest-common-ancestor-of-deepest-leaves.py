7bd8f2c4befda3d8705cd451b411f5be870cb0dd
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-03-18 09:21:26 +0300

    Time: 50 ms (68.48%), Space: 14.5 MB (28.81%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.py
@@ -1,23 +1,44 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
 class Solution:
-    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(cur):
-            if not cur:
-                return [None, 0]
+    def lcaDeepestLeaves(self, root: TreeNode) -> TreeNode:
+        deepest_leaves = []
+        max_depth = 0
 
-            left = dfs(cur.left)
-            right = dfs(cur.right)
+        def dfs(node, depth):
+            nonlocal deepest_leaves, max_depth
+            if not node:
+                return 0
 
-            if left[1] == right[1]:
-                return [cur, left[1] + 1]
-            elif left[1] > right[1]:
-                return [left[0], left[1] + 1]
+            left_depth = dfs(node.left, depth + 1)
+            right_depth = dfs(node.right, depth + 1)
+
+            if left_depth == right_depth and left_depth + depth >= max_depth:
+                if left_depth + depth > max_depth:
+                    deepest_leaves = []
+                max_depth = left_depth + depth
+                deepest_leaves.append(node)
+
+            return max(left_depth, right_depth) + 1
+
+
+        def find_lca(node, depth):
+            if not node or depth == max_depth:
+                return node, depth
+
+            left_lca, left_depth = find_lca(node.left, depth + 1)
+            right_lca, right_depth = find_lca(node.right, depth + 1)
+
+            if left_lca and right_lca:
+                return node, depth
+            elif left_lca:
+                return left_lca, left_depth
             else:
-                return [right[0], right[1] + 1]
-            
-        return dfs(root)[0]
+                return right_lca, right_depth
+        
+        dfs(root, 0)
+        return find_lca(root, 0)[0]
