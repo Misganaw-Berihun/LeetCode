0be938622196fb3fd5e44476f2dc634443583586
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-03-27 19:30:57 +0300

    Time: 2801 ms (12.14%), Space: 32.5 MB (86.64%) - LeetHub

--- 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.py
@@ -1,10 +1,46 @@
-from sortedcontainers import *
 class Solution:
-    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
-        arr = SortedList()
-        ans = 0
+    def merge(self, arr, left, mid, right, diff):
+        new_list = []
+        p1,p2 = left, mid + 1
         
-        for a, b in zip(nums1, nums2):
-            ans += arr.bisect_right(a - b + diff)
-            arr.add(a - b)
-        return ans
+        while p1 <= mid:
+            while p2 <= right and arr[p1] > arr[p2] + diff:
+                p2 += 1
+            
+            self.count += (right - p2 + 1)
+            p1 += 1
+        
+        p1, p2 = left, mid + 1
+        while p1 <= mid or p2 <= right:
+            if p2 == right + 1 or (p1 <= mid and arr[p1] < arr[p2]):
+                new_list.append(arr[p1])
+                p1 += 1
+            else:
+                new_list.append(arr[p2])
+                p2 += 1
+        
+        p1 = left
+        for i in range(len(new_list)):
+            arr[p1] = new_list[i]
+            p1 += 1
+        
+    def mergeSort(self,arr,left, right, diff):
+        if left == right:
+            return 
+        
+        mid = (left + right) // 2
+        self.mergeSort(arr, left, mid, diff)
+        self.mergeSort(arr, mid + 1, right, diff)
+        self.merge(arr, left, mid,  right, diff)
+        
+        
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        n = len(nums1)
+        new_arr = []
+        
+        for i in range(n):
+            new_arr.append(nums1[i] - nums2[i])
+        
+        self.count = 0
+        self.mergeSort(new_arr, 0, n - 1, diff)
+        return self.count
