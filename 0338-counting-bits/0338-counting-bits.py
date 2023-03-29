abaa70854bc48ce4957fd6f3c5d3da1f1498c9c5
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-03-29 19:33:39 +0300

    Time: 198 ms (11.80%), Space: 20.8 MB (66.00%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,8 +3,9 @@ class Solution:
         answer = []
         for i in range(n + 1):
             count = 0
-            for j in range(32):
-                if ((1 << j) & i) > 0:
-                    count += 1
+            while i != 0:
+                count += i & 1
+                i >>= 1
+                
             answer.append(count)
         return answer
