d4cbc408ba3aba2b386af832938a41ded5b60184
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-04-06 23:52:08 +0300

    Time: 346 ms (31.97%), Space: 21.8 MB (24.89%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -0,0 +1,39 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def bfs(row,col):
+            queue = deque()
+            queue.append((row,col))
+            visited.add((row,col))
+        
+            while queue:
+                l = len(queue)
+                
+                cur = queue.popleft()
+
+                for d in direction:
+                    new_row = cur[0] + d[0]
+                    new_col = cur[1] + d[1]
+
+                    if in_bound(new_row,new_col) and (new_row,new_col) not in visited and \
+                                        grid[new_row][new_col] == "1":
+                        visited.add((new_row,new_col))
+                        queue.append((new_row,new_col))
+                                
+        visited = set()
+        n = len(grid)
+        m = len(grid[0])
+        direction = [[0,1],[1,0],[-1,0],[0,-1]]
+        in_bound = lambda row,col: 0 <= row < n and 0 <= col < m
+        islands = 0
+        
+        for i in range(n):
+            for j in range(m):
+                if (i,j) not in visited and grid[i][j] == "1":
+                    bfs(i,j)
+                    islands += 1
+                    
+        return islands
+                                
+                        
+                            
+        
