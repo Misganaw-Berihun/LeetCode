ce3193eb1c8ea859d28d49d858e8b53aa67945da
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-04-07 13:36:27 +0300

    Time: 1430 ms (83.97%), Space: 117.7 MB (34.62%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,38 @@
+class Solution:
+    def hasValidPath(self, grid: List[List[int]]) -> bool:
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = [[False] * COLS for j in range(ROWS)]
+        inbound = lambda r, c: 0 <= r < ROWS and 0 <= c < COLS
+        
+        directions = {
+                1: [(0, 1), (0, -1)],
+                2: [(1, 0), (-1, 0)],
+                3: [(1, 0), (0, -1)],
+                4: [(1, 0), (0, 1)],
+                5: [(0, -1), (-1, 0)],
+                6: [(-1, 0), (0, 1)]
+        }
+        
+        def dfs(row, col):
+            if row == ROWS - 1 and col == COLS - 1:
+                return True
+            
+            visited[row][col] = True
+            for dx, dy in directions[grid[row][col]]:
+                nx, ny = row + dx, col + dy
+                
+                
+                if not inbound(nx, ny):
+                    continue
+                    
+                if visited[nx][ny]:
+                    continue
+                
+                if (-dx, -dy) in directions[grid[nx][ny]]:
+                    if dfs(nx, ny):
+                        return True
+            
+            return False
+        
+        return dfs(0, 0)
+                    
