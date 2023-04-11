f7af4a6d7f579b5bfc9633c2b51a7093ac6b7417
Misganaw-Berihun <85707242+Misganaw-Berihun@users.noreply.github.com>
2023-04-11 18:52:56 +0300

    Time: 845 ms (52.99%), Space: 17.4 MB (30.63%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,27 @@
+class Solution:
+    def musk(self, word):
+        num = 0
+        
+        for ch in word:
+            shift = ord(ch) - ord('a')
+            num = num | (1 << shift)
+        
+        return num
+        
+    def maxProduct(self, words: List[str]) -> int:
+        arr = []
+        temp1 = 0
+        temp2 = 0
+        
+        #cccce
+        #dbaaffdfa
+        for word in words:
+            cur_musk = self.musk(word)
+            arr.append((cur_musk, len(word)))
+        
+        ans = 0
+        for el1 in arr:
+            for el2 in arr:
+                if (el1[0] & el2[0]) == 0:
+                    ans = max(ans, el1[1] * el2[1])
+        return ans
